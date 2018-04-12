cf83bd88c7318b721b92000a7733b1961e37c1ed
prem0406 <premk191@gmail.com>
2018-04-12 10:01:16 +0530

    Modified

--- DemandPage.c
+++ DemandPage.c
@@ -2,5 +2,19 @@
 #include<stdio.h>
 int main()
 {
+	long t_pservice,t_preplace,t_maccess,t_eaccess;
+	float p_pservice,p_preplace;
+	double p;
+	t_pservice=8000000;
+	t_preplace=20000000;
+	p_preplace=0.7;
+	p_pservice=1-p_preplace;
+	t_maccess=100;
+	t_eaccess=200;
+	p=(t_eaccess-t_maccess)/(t_pservice*p_pservice+t_preplace*p_preplace-t_maccess);
+	printf("Page Fault Rate: %lf \n",p*100);
+
+	//printf("Page Fault Rate: %lf \n",p);
+	
 
 }
